93f33866cc668d3ac63354332b6bfe0c12e9b056
Mateus Schverz <mate_fs@hotmail.com>
2022-11-06 20:39:23 -0300

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+      integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+      crossorigin="anonymous"
+    />
+   
+    <div class="container .container-fluid">
+      <dataTable />
+    </div>
+   
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import dataTable from './components/dataTable.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    dataTable
   }
 }
 </script>
---
+++ src/components/dataTable.vue
@@ -0,0 +1,44 @@
+<template>
+   <div class="container">
+    <h1>Tabela consultando API</h1>
+    <table class="table table-dark table-hover">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Nome</th>
+          <th scope="col">Email</th>
+          <th scope="col">Cidade</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="user in usuarios" :key="user.id">
+          <th scope="row">#</th>
+          <td>{{ user.name }}</td>
+          <td>{{ user.email }}</td>
+          <td>{{ user.address.city }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: 'dataTable',
+  data() {
+    return {
+      usuarios: String,
+    }
+  },
+  mounted() {
+    axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
+      this.usuarios = res.data;
+      console.log(res.data);
+    });
+  },
+
+}
+</script>
+
+ 
